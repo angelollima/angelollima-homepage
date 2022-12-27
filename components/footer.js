6daf03bd9aee564f874ebbdb9a0e94005b1cab70
angelollima <angeloantonio442@gmail.com>
2022-12-26 22:07:10 -0300

    Adding footer.js file

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='text-center my-10'>
+      &copy; {new Date().getFullYear()} <span>Angelo Lima</span> All Rights Reserved!
+    </footer>
+  )
+}
+
+export default Footer
