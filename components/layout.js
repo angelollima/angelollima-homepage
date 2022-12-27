043c1b8a4c91cde6f7f2644fff3c2718461426b1
angelollima <angeloantonio442@gmail.com>
2022-12-26 22:42:19 -0300

    Removing font

--- components/layout.js
+++ components/layout.js
@@ -8,9 +8,6 @@ const Layout = ({ children }) => {
         <title>Angelollima - HomePage</title>
         <meta name="description" content="Portfolio about Angelo Lima, a junior developer always looking for development. This site was generated with Next.js" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@900&display=swap" rel="stylesheet" />
       </Head>
 
     <div className='min-h-screen'>
