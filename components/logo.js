b67767a3fb75ee923c0e870bed7705662de4687e
angelollima <angeloantonio442@gmail.com>
2022-12-26 22:08:48 -0300

    Adding logo.js file

---
+++ components/logo.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+const Logo = () => {
+  return (
+    <Link href="/">
+      <p
+        className='text-sm ml-4 font-MPlus font-extrabold'
+      >
+        Angelo Lima
+      </p>
+    </Link>
+  )
+}
+
+export default Logo
